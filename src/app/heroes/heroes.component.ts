35638d5bf47f36ae3fc0a6d0f2409f411ad9b90b
Алексей Топунов <telefoniero@github.com>
2024-01-30 23:03:47 +0300

    3.5 step of Tour of Heroes angular tutor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,5 +13,15 @@ export class HeroesComponent {
   onSelect(hero: Hero) {
     this.selectedHero = hero;
   }
-  heroes = HEROES;
+  heroes?: Hero[];
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes() {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
 }
